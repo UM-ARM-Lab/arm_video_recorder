f20be826f4ad1d4417fa14ce280c0e3b17545837
Brad Saund <brad.saund@gmail.com>
2019-01-25 20:21:26 -0500

    Change arc to arm

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['arc_video_recorder'],
+    packages=['arm_video_recorder'],
     package_dir={'': 'src'}
 )
 setup(**d)

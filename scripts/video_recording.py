c52bfdb6d42af8000ba3a38c437e6a169762232f
Brad Saund <brad.saund@gmail.com>
2019-01-25 20:22:24 -0500

    Changed recording function to properly handle stops. Note: Not threadsafe yet (and messages are threaded)

---
+++ scripts/video_recording.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+import cv2
+import rospy
+from std_msgs.msg import String
+from arm_video_recorder.srv import TriggerVideoRecording, TriggerVideoRecordingResponse
+
+
+record_srv_cmd = False
+start_new_recording = False
+stop_current_recording = False
+out_filename = None
+
+        
+def video_service_callback(req):
+    global start_new_recording
+    global stop_current_recording
+    global out_filename
+
+    out_filename = req.filename
+    
+    start_new_recording = req.record
+    stop_current_recording = True
+    
+    resp = TriggerVideoRecordingResponse()
+
+    resp.success = True
+    return resp
+
+
+
+if __name__== "__main__":
+    rospy.init_node("video_recorder")
+    s = rospy.Service('video_recorder', TriggerVideoRecording, video_service_callback)
+
+    cap = cv2.VideoCapture("sample_video.mp4")
+
+    if(not cap.isOpened()):
+        print("Error opening video stream or file")
+
+    frame_dims = (int(cap.get(3)), int(cap.get(4)))
+
+    rospy.loginfo("Video Recorder ready")
+
+    recording = False
+    out = None
+    while not rospy.is_shutdown():
+        if stop_current_recording:
+            recording = False
+            stop_current_recording = False
+            rospy.loginfo("Stopping any existing recordings")
+            if out is not None:
+                out.release()
+            
+        if start_new_recording:
+            recording = True
+            start_new_recording = False
+            rospy.loginfo("Starting recording for " + out_filename)
+            out = cv2.VideoWriter(out_filename,
+                                  0x00000021,
+                                  30,
+                                  frame_dims)
+
+        if not recording:
+            rospy.sleep(0.1)
+            continue
+
+
+        ret, frame = cap.read()
+        if not ret:
+            rospy.logerr("Camera capture failed")
+            break
+        
+        out.write(frame)
+
+    out.release()
+    cap.release()
+

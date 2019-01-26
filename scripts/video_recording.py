6acf27911a8588981fc52135071c15416ce232ad
Brad Saund <brad.saund@gmail.com>
2019-01-26 11:49:41 -0500

    Changed the implementation to a video recorder class for cleaner implementation Add a timelimit

--- scripts/video_recording.py
+++ scripts/video_recording.py
@@ -4,31 +4,77 @@ import rospy
 from std_msgs.msg import String
 from arm_video_recorder.srv import TriggerVideoRecording, TriggerVideoRecordingResponse
 from threading import Lock
+import time
 
-record_srv_cmd = False
-start_new_recording = False
-stop_current_recording = False
-out_filename = None
 
-lock = Lock()
+class VideoRecorder:
+    def __init__(self, cap):
+        self.out_filename = None
+        self.lock = Lock()
+        self.is_recording = False
+        self.s = rospy.Service('video_recorder', TriggerVideoRecording, self.srv_trigger_recording_callback)
+
+        self.out = None
+        self.cap = cap
+        self.record_start_time = time.time()
+        self.record_time_limit = 0
+
+    def stop_current_recording(self):
+        if self.out is not None:
+            self.out.release()
+        if self.is_recording:
+            rospy.loginfo("Stopping current recording")
+        self.is_recording = False
+
+    def start_new_recording(self, filename):
+        self.is_recording = True
+        self.record_start_time = time.time()
+        rospy.loginfo("Starting recording for " + filename)
+        frame_dims = (int(self.cap.get(3)), int(self.cap.get(4)))
+        self.out = cv2.VideoWriter(filename,
+                                   0x00000021,
+                                   30,
+                                   frame_dims)
+
+    def srv_trigger_recording_callback(self, req):
+        resp = TriggerVideoRecordingResponse()
+        resp.success = True
+        with self.lock:
+            self.stop_current_recording()
+            self.record_time_limit = req.timeout_in_sec
+            if(req.record):
+                self.start_new_recording(req.filename)
+            
+        if(self.record_time_limit <= 0):
+            resp.success = False
+            rospy.logerr("Cannot record with a timeout of 0s")
+
+        return resp
+
+    def save_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            rospy.logerr("Camera capture failed")
+            self.stop_current_recording()
+            return
+        self.out.write(frame)
+
+    def check_timeout(self):
+        if time.time() - self.record_start_time > self.record_time_limit:
+            rospy.loginfo("Record time limit of " + str(self.record_time_limit) + " has been reached")
+            self.stop_current_recording()
+            return False
+        return True
         
-def video_service_callback(req):
-    global start_new_recording
-    global stop_current_recording
-    global out_filename
-    global lock
 
-    with lock:
-        out_filename = req.filename
-        
-        start_new_recording = req.record
-        stop_current_recording = True
-    
-    resp = TriggerVideoRecordingResponse()
+    def work_in_loop(self):
+        with self.lock:
+            if self.is_recording and self.check_timeout():
+                self.save_frame()
+                return
 
-    resp.success = True
-    return resp
 
+            
 
 
 if __name__== "__main__":
@@ -41,45 +87,17 @@ if __name__== "__main__":
         rospy.logerr("Error opening video stream or file")
         exit()
 
-    s = rospy.Service('video_recorder', TriggerVideoRecording, video_service_callback)
-
-    frame_dims = (int(cap.get(3)), int(cap.get(4)))
-
+        
     rospy.loginfo("Video Recorder ready")
 
-    recording = False
-    out = None
+    vr = VideoRecorder(cap)
+
     while not rospy.is_shutdown():
-        with lock:
-            if stop_current_recording:
-                stop_current_recording = False
-                if recording:
-                    rospy.loginfo("Stopping current recording")
-                recording = False
-                if out is not None:
-                    out.release()
-            
-            if start_new_recording:
-                recording = True
-                start_new_recording = False
-                rospy.loginfo("Starting recording for " + out_filename)
-                out = cv2.VideoWriter(out_filename,
-                                      0x00000021,
-                                      30,
-                                      frame_dims)
-
-        if not recording:
-            rospy.sleep(0.1)
-            continue
+        vr.work_in_loop()
+        rospy.sleep(0.01) # Don't eat up 100%CPU in the while loop
 
 
-        ret, frame = cap.read()
-        if not ret:
-            rospy.logerr("Camera capture failed")
-            break
-        
-        out.write(frame)
-
-    out.release()
     cap.release()
+
+
 
